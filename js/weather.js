82173085b9fd21b0cf1bc1edc045dcd3f437a25f
David Juarez <djuar05@gmail.com>
2020-04-17 13:05:27 -0400

    weather with zipcode

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,36 @@
 //api key for open weather
 //32e202b1d9f0b78dbf8cc5854ff951a2
 
-// alert('hi')
+
 var Moment = require('moment');
 
-var time = new Moment();
-console.log(time.format('h:mm:ss a'))
 
 
 $('#submitZipcode').on('click', function(){
-    console.log("button pushed")
+    let zipcode = $('#zipcodeSubmit').val();
+    console.log(zipcode);
+    $.get('https://api.openweathermap.org/data/2.5/weather?zip=' +zipcode + '&appid=32e202b1d9f0b78dbf8cc5854ff951a2',function(response){
+        $('#zipcodeSumit').empty;
+        console.log(response.main.temp);
+        let cityWeather = {
+            currentName: response.name,
+            currentWeather: response.weather[0].description,
+            temp: Math.floor((response.main.temp - 273.15) * (9/5) + 32),
+            // convertedTempToFahrenheit: Math.floor((temp - 273.15) * (9/5) + 32)
+        };
+        console.log(cityWeather);
+        $('#displayJsTime').append(cityWeather)
+        // .append('Welcome to ' + currentName + '<br>')
+        // .append('where the current temperature is ' + convertedTempToFahrenheit+ 'degress F' + '<br>')
+        // .append(currentWeather);
+    });
+
+    
+    
+    
+    
+    // let time = new Moment();
+    // currentTime = time.format('h:mm:ss a');
+
+
 })
