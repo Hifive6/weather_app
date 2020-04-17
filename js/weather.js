66135e698f129635ce5f4a182fa0ae89c2d21a5c
David Juarez <djuar05@gmail.com>
2020-04-17 13:53:03 -0400

    information is not object

--- js/weather.js
+++ js/weather.js
@@ -8,18 +8,23 @@ var Moment = require('moment');
 
 $('#submitZipcode').on('click', function(){
     let zipcode = $('#zipcodeSubmit').val();
-    console.log(zipcode);
+    
     $.get('https://api.openweathermap.org/data/2.5/weather?zip=' +zipcode + '&appid=32e202b1d9f0b78dbf8cc5854ff951a2',function(response){
         $('#zipcodeSumit').empty;
-        console.log(response.main.temp);
+        
         let cityWeather = {
-            currentName: response.name,
-            currentWeather: response.weather[0].description,
+            name: response.name,
+            weather: response.weather[0].description,
             temp: Math.floor((response.main.temp - 273.15) * (9/5) + 32),
             // convertedTempToFahrenheit: Math.floor((temp - 273.15) * (9/5) + 32)
         };
-        console.log(cityWeather);
-        $('#displayJsTime').append(cityWeather)
+
+            $.each(cityWeather, function(key, val){
+                console.log(key);
+                console.log(val);
+            })
+        // console.log(cityWeather);
+        $('#displayJsTime').append(cityWeather.name)
         // .append('Welcome to ' + currentName + '<br>')
         // .append('where the current temperature is ' + convertedTempToFahrenheit+ 'degress F' + '<br>')
         // .append(currentWeather);
