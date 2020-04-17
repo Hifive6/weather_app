3de964e88c5628e1e683138e051906f4dcd9283b
David Juarez <djuar05@gmail.com>
2020-04-17 09:18:13 -0400

    npm packages environment

---
+++ js/weather.js
@@ -0,0 +1,13 @@
+//api key for open weather
+//32e202b1d9f0b78dbf8cc5854ff951a2
+
+// alert('hi')
+var Moment = require('moment');
+
+var time = new Moment();
+console.log(time.format('h:mm:ss a'))
+
+
+$('#submitZipcode').on('click', function(){
+    console.log("button pushed")
+})
